51c3e34aa304000f9ac01855d28e404e3270f8b3
BrianWu <brianwu291@gmail.com>
2021-09-29 11:38:38 +0800

    add comment to testReplaySubject.

--- src/testReplaySubject.js
+++ src/testReplaySubject.js
@@ -25,6 +25,7 @@ export default function testReplaySubject() {
 
   // const recentEventCount = 2, windowTime = 3000;
   // const source$ = createReplaySubject(recentEventCount, windowTime);
+  // 表示事件發生後的 3000 ms 後，就不再保留事件資料了
 
   // source$.subscribe((data) => {
   //   console.log(`ReplaySubject 第一次訂閱: ${data}`);
