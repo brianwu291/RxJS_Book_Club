6568c31285c507d7a94df28b0b34c5c6123f8290
BrianWu <brianwu291@gmail.com>
2021-10-20 18:12:12 +0800

    add xstate.

--- src/index.js
+++ src/index.js
@@ -8,31 +8,9 @@ import ReactDOM from 'react-dom';
 // import testAsyncSubject from './testAsyncSubject';
 // import testAsObservable from './testAsObservable';
 // import testWarmObservable from './testWarmObservable';
-import memberOne from './memberOne';
+// import memberOne from './memberOne';
 
 function App() {
-  memberOne();
-  // #Observable
-  // testObservable();
-
-  // #Subject
-  // testSubject();
-
-  // #BehaviorSubject
-  // testBehaviorSubject();
-
-  // #ReplaySubject
-  // testReplaySubject();
-
-  // #AsyncSubject
-  // testAsyncSubject();
-
-  // #AsObservable
-  // testAsObservable();
-
-  // #WarmObservable
-  // testWarmObservable();
-
   return (
     <div>
       App
