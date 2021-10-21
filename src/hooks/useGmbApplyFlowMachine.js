8cb58930174883aefa153c22866847b33ebb2428
BrianWu <brianwu291@gmail.com>
2021-10-22 00:32:43 +0800

    finish overall flow.

--- src/hooks/useGmbApplyFlowMachine.js
+++ src/hooks/useGmbApplyFlowMachine.js
@@ -1,32 +1,194 @@
+import { useState } from 'react';
+
 import { useMachine } from '@xstate/react';
-import { Machine } from 'xstate';
+import { createMachine, assign } from 'xstate';
 
-const googleLoginStates = {
-  initial: 'fetchingOTC',
-  context: {
-    oneTimeCode: null,
-    errorMessage: null,
-    email: null,
-  },
-  states: {
-    logging: 'logging',
-    loginSuccess: 'loginSuccess',
-    userDenyAccess: 'userDenyAccess',
-  },
+export const APPLY_EVENTS = {
+  toReady: 'toReady',
+  toAgreeTerm: 'toAgreeTerm',
+  toWarnThirdPartyCookieBlocked: 'blockThirdPartyCookie',
+  toGoogleLogin: 'toGoogleLogin',
+  toGmbApply: 'toGmbApply',
+  hasExistingLocation: 'toSelectExistingLocations',
+  noExistingLocation:  'toCreateNewLocation',
+  toCreateNewLocation: 'toCreateNewLocation',
 };
 
-const applyStates = {
-  unApply: 'unApply',
-  term: 'term',
-  googleLogin: googleLoginStates,
-
-};
-
-const gmbApplyFlowMachine = Machine({
-  initial: 'start',
-  states: applyStates,
+const getOTC = () => new Promise((res) => {
+  setTimeout(() => {
+    res({
+      oneTimeCode: null,
+      error: 'user deny',
+    });
+    // const random = Math.random() * 5;
+    // if (random >= 2) {
+    //   res({
+    //     oneTimeCode: 'one time code',
+    //     error: null,
+    //   });
+    // } else {
+    //   res({
+    //     oneTimeCode: null,
+    //     error: 'user deny',
+    //   });
+    // }
+  }, 1000);
 });
 
+const googleLoginStates = {
+  type: 'compound',
+  initial: 'loading',
+  states: {
+    loading: {
+      invoke: {
+        id: 'login',
+        src: async ({ loginMutate }) => {
+          const {
+            oneTimeCode,
+            error,
+          } = await getOTC();
+          if (oneTimeCode) {
+            const email = await loginMutate(oneTimeCode);
+            return ({
+              email,
+              oneTimeCode,
+            });
+          }
+          throw ({
+            error,
+            email: null,
+            oneTimeCode: null,
+          });
+        },
+        onDone: {
+          target: 'success',
+          actions: ['onLoginSuccess'],
+        },
+        onError: {
+          target: 'failure',
+          actions: ['onLoginFail'],
+        },
+      },
+    },
+    success: {
+      on: {
+        [APPLY_EVENTS.toGmbApply]: {
+          target: '#gmb.applyGmb',
+        },
+      },
+    },
+    failure: {
+      on: {
+        [APPLY_EVENTS.toReady]: {
+          target: '#gmb.ready',
+        },
+        [APPLY_EVENTS.toAgreeTerm]: {
+          target: '#gmb.agreeTerm',
+        },
+        [APPLY_EVENTS.toWarnThirdPartyCookieBlocked]: {
+          target: '#gmb.blockThirdPartyCookie'
+        },
+      }
+    },
+  },
+};
+
+const applyGmbState = {
+  type: 'compound',
+  initial: 'fetchingExistingLocations',
+  states: {
+    fetchingExistingLocations: {
+      on: {
+        [APPLY_EVENTS.hasExistingLocation]: {
+          target: 'selectExistingLocations',
+          actions: ['onReceiveExistingLocations'],
+        },
+        [APPLY_EVENTS.noExistingLocation]: {
+          target: 'createNewLocation',
+          actions: ['onReceiveExistingLocations'],
+        },
+      },
+    },
+    selectExistingLocations: {
+      on: {
+        [APPLY_EVENTS.toCreateNewLocation]: 'createNewLocation',
+      },
+    },
+    createNewLocation: {},
+  },
+};
+
+const applyStepStates = {
+  ready: {
+    on: {
+      [APPLY_EVENTS.toAgreeTerm]: 'agreeTerm',
+    },
+  },
+  agreeTerm: {
+    on: {
+      [APPLY_EVENTS.toGoogleLogin]: 'googleLogin',
+    },
+  },
+  blockThirdPartyCookie: {
+    on: {
+      [APPLY_EVENTS.toReady]: 'ready',
+    },
+  },
+  googleLogin: googleLoginStates,
+  applyGmb: applyGmbState,
+};
+
+function useGoogleUserLoginMutation() {
+  const [result, setResult] = useState('');
+  const mutate = () => new Promise((res) => {
+    setTimeout(() => {
+      res('sdzse@g,ail.com');
+      setResult('sdzse@g,ail.com');
+    }, 700);
+  });
+  return [mutate, result];
+}
+
 export default function useGmbApplyFlowMachine(options) {
-  return useMachine(gmbApplyFlowMachine, options);
+  const [loginMutate] = useGoogleUserLoginMutation();
+  const gmbApplyFlowMachine = createMachine({
+    id: 'gmb',
+    initial: 'ready',
+    context: {
+      oneTimeCode: null,
+      errorMessage: null,
+      email: null,
+      existingLocations: null,
+      loginMutate
+    },
+    states: applyStepStates,
+  });
+  return useMachine(
+    gmbApplyFlowMachine,
+    {
+      actions: {
+        onLoginSuccess: assign({
+          oneTimeCode: (_, { data }) => data.oneTimeCode,
+          email: (_, { data }) => data.email,
+        }),
+        onLoginFail: assign({
+          errorMessage: (_, { data }) => data.error,
+        }),
+        onReceiveExistingLocations: assign({
+          existingLocations: (_, event) => {
+            if (event.type === APPLY_EVENTS.hasExistingLocation) {
+              return [
+                {
+                  name: 'location-one',
+                  id: 'location-one-id',
+                },
+              ]
+            }
+            return [];
+          }
+        }),
+      },
+      ...options,
+    },
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import useGmbApplyFlowMachine, {
+  APPLY_EVENTS,
+} from './hooks/useGmbApplyFlowMachine';
+
 // import testObservable from './testObservable';
 // import testSubject from './testSubject';
 // import testBehaviorSubject from './testBehaviorSubject';
@@ -11,9 +15,19 @@ import ReactDOM from 'react-dom';
 // import memberOne from './memberOne';
 
 function App() {
+  const [state, send] = useGmbApplyFlowMachine();
+  console.log('current state', state.value);
+  console.log('current context', state.context);
   return (
     <div>
       App
+      <button
+        type="button"
+        onClick={() => send(APPLY_EVENTS.next)}
+      >
+        next
+      </button>
+      {}
     </div>
   );
 }
